af21533fa2a210f3dd6f606189ee92501e2d89c3
Steve NGANDA <steveloicnganda@gmail.com>
2025-05-31 22:10:25 +0100

    edge runtime enable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,70 +4,6 @@ import localFont from 'next/font/local'
 import "@/css/style.css";
 import { ToasterProvider } from "../components/ui/toaster";
 
-const glancyr = localFont({
-  src: [
-    {
-      path: './../fonts/glancyr-glancyr-extra-light-100.ttf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-extra-light-italic-100.ttf',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-light-200.ttf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-light-italic-200.ttf',
-      weight: '200',
-      style: 'italic',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-regular-400.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-regular-italic-400.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-medium-500.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-medium-italic-500.ttf',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-semibold-600.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-semibold-italic-600.ttf',
-      weight: '600',
-      style: 'italic',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-bold-700.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './../fonts/glancyr-glancyr-bold-italic-700.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-  ],
-})
 
 export default function RootLayout({
   children,
@@ -86,3 +22,6 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+export const runtime = "edge";
